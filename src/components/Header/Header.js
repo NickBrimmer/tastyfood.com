a4acdbe8b8db0792415e57538ad129bf140f2ce2
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-08-20 16:05:21 -0700

    href for seasonal id changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
           <li><a href="#featured" >Featured</a></li>
           <li><a href="#cuisines" >Cuisines</a></li>
           <li><a href="#about" >About</a></li>
-          <li><a href="#" >Seasonal</a></li>
+          <li><a href="#seasonal" >Seasonal</a></li>
         </ul>
       </div>
       <div className="logoBox">
