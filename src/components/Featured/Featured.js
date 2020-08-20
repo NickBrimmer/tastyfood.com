75f2602da0a33daa38581cfcf3278754223d0da9
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-08-20 15:41:19 -0700

    added Feature section

---
+++ src/components/Featured/Featured.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from '../../styles.css';
+
+const Featured = () => {
+
+  return (
+    <section id="featured" className={styles.featureSection}>
+      <h2 className={styles.featuredText}>FEATURED RECIPES</h2>
+      <div>
+        <img />
+      </div>
+    </section>
+  );
+};
+
+export default Featured;
