857b9111394779062b6a1728a014ffd669a50021
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-08-20 17:29:12 -0700

    features section cards constructed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './styles/App.css';
-import Header from './components/Header/Header';
+import Homepage from './containers/Homepage';
 
 
 
 export default function App() {
-  return <Header />;
+  return <Homepage />;
 }
 
--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import Featured from '../components/Featured/Featured';
 
 const Homepage = () => {
   return (
     <section>
       <Header />
-
+      <Featured />
     </section>
   );
 };
